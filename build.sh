48483bdc5c709df8e983356b2b911c78efb69bec
Developer <developer@MAC-W229.local>
2019-12-10 21:57:17 +0100

    Got patches working

--- build.sh
+++ build.sh
@@ -1,13 +1,33 @@
 #!/usr/bin/env bash
 
 # Hard-crash everything
-set -ex
+set -e
 
 # Download preprocessor
 [ -f "bashpp" ] || {
   curl -Ls https://raw.githubusercontent.com/iwonbigbro/bashpp/master/bin/bashpp > bashpp
   chmod +x bashpp
 }
+
+# Remove old patches
+rm -rf inc/patches
+
+# Include all patches
+mkdir inc/patches
+find patches | while read filename; do
+  if [ -d $filename ]; then
+    continue
+  fi
+  mkdir -p inc/$(dirname $filename)
+  echo "#include <$filename>" >> inc/patches/all
+  version=$(basename $(dirname $filename))
+  minor=$(echo "${version}" | tr '.' ' ' | awk '{print $2}')
+  major=$(echo "${version}" | tr '.' ' ' | awk '{print $1}')
+  CONTENTS="$(cat $filename | base64 --wrap 0)"
+  echo "if [ \"\${major}\" -eq \"${major}\" ] && [ \"\${minor}\" -eq \"${minor}\" ]; then" >> inc/$filename
+  echo "PATCHES+=(\"$CONTENTS\")"                                                          >> inc/$filename
+  echo "fi"                                                                                >> inc/$filename
+done
 
 ./bashpp -I inc -o dist/phpvm src/phpvm 
 chmod +x dist/phpvm
