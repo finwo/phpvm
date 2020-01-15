1402cadb7fa59ea2a04f765914861477a57d02b7
Robin Bron <finwo@pm.me>
2020-01-15 11:40:53 +0100

    removed everything

--- build.sh
+++ build.sh
@@ -9,25 +9,26 @@ set -e
   chmod +x bashpp
 }
 
-# Remove old patches
-rm -rf inc/patches
+# # Remove old patches
+# rm -rf inc/patches
 
-# Include all patches
-mkdir inc/patches
-find patches | sort | while read filename; do
-  if [ -d $filename ]; then
-    continue
-  fi
-  mkdir -p inc/$(dirname $filename)
-  echo "#include <$filename>" >> inc/patches/all
-  version=$(basename $(dirname $filename))
-  minor=$(echo "${version}" | tr '.' ' ' | awk '{print $2}')
-  major=$(echo "${version}" | tr '.' ' ' | awk '{print $1}')
-  CONTENTS="$(cat $filename | base64 --wrap 0)"
-  echo "if [ \"\${major}\" -eq \"${major}\" ] && [ \"\${minor}\" -eq \"${minor}\" ]; then" >> inc/$filename
-  echo "PATCHES+=(\"$CONTENTS\")"                                                          >> inc/$filename
-  echo "fi"                                                                                >> inc/$filename
-done
+# # Include all patches
+# mkdir inc/patches
+# find patches | sort | while read filename; do
+#   if [ -d $filename ]; then
+#     continue
+#   fi
+#   mkdir -p inc/$(dirname $filename)
+#   echo "#include <$filename>" >> inc/patches/all
+#   version=$(basename $(dirname $filename))
+#   minor=$(echo "${version}" | tr '.' ' ' | awk '{print $2}')
+#   major=$(echo "${version}" | tr '.' ' ' | awk '{print $1}')
+#   CONTENTS="$(cat $filename | base64 --wrap 0)"
+#   echo "if [ \"\${major}\" -eq \"${major}\" ] && [ \"\${minor}\" -eq \"${minor}\" ]; then" >> inc/$filename
+#   echo "PATCHES+=(\"$CONTENTS\")"                                                          >> inc/$filename
+#   echo "fi"                                                                                >> inc/$filename
+# done
 
-./bashpp -I inc -o dist/phpvm src/phpvm 
+mkdir -p dist
+./bashpp -o dist/phpvm src/phpvm 
 chmod +x dist/phpvm
